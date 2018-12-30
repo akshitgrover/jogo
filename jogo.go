bcda36510a63a34b189c44c5aae0c9d2905c17ed
Akshit Grover <akshit.grover2016@gmail.com>
2018-12-30 17:35:26 +0530

    JoGO: Add GetType method

--- jogo.go
+++ jogo.go
@@ -2,6 +2,8 @@ package jogo
 
 import (
 	"encoding/json"
+	"errors"
+	"reflect"
 )
 
 type ExportedJson struct {
@@ -11,6 +13,7 @@ type ExportedJson struct {
 
 type ResultJson struct {
 	rawJson interface{}
+	Type    string
 }
 
 func Export(importedJson string) (ExportedJson, ResultJson, error) {
@@ -24,9 +27,31 @@ func Export(importedJson string) (ExportedJson, ResultJson, error) {
 
 	var resJson ResultJson
 	resJson.rawJson = expJson.rawJson
+	resJson.Type, err = GetType(expJson.rawJson)
 	if err != nil {
 		return ExportedJson{}, ResultJson{}, err
 	}
 
 	return expJson, resJson, nil
+}
+
+func GetType(v interface{}) (string, error) {
+
+	switch reflect.ValueOf(v).Type().String() {
+	case "map[string]interface {}":
+		return "OBJECT", nil
+	case "[]interface {}":
+		return "LIST", nil
+	case "string":
+		return "STRING", nil
+	case "float64":
+		return "NUMBER", nil
+	case "bool":
+		return "BOOLEAN", nil
+	case "":
+		return "", errors.New("InvalidArg: Invalid argument passed in GetType method.")
+	default:
+		return "", nil
+	}
+
 }
